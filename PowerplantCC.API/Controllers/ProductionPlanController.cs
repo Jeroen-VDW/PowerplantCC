a6e1195fee527a2a1eb28b41c311210b8b5c7809
Jeroen van de Woestyne <jeroenwo@Ortec.Com>
2023-11-14 21:59:16 +0100

    Create project

---
+++ PowerplantCC.API/Controllers/ProductionPlanController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PowerplantCC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductionPlanController : ControllerBase
+    {
+
+    }
+}
